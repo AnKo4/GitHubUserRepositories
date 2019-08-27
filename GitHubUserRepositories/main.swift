c0824796a7940cdb4d1751c87701ea96bef68191
Andrey <andrey@Andreys-Mac.local>
2019-08-27 14:10:53 +0400

    Initial Commit

---
+++ GitHubUserRepositories/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  GitHubUserRepositories
+//
+//  Created by Andrey on 27/08/2019.
+//  Copyright © 2019 Andrey Kochetkov. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
