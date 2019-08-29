94ec72cec8125e7611edcdb6eb784ef30d547faa
Andrey <andrey@Andreys-Mac.local>
2019-08-29 13:24:18 +0400

    Add comments

--- Sources/GitHubUserRepositories/main.swift
+++ Sources/GitHubUserRepositories/main.swift
@@ -3,7 +3,7 @@ import Alamofire
 
 var urlString = "https://api.github.com/users/"
 
-
+// MARK: - Запрашиваем никнейм до тех пор? пока пользователь что-нибудь не введет
 func readName() -> String {
     var name: String?
     repeat {
@@ -18,20 +18,25 @@ func readName() -> String {
     return userName
 }
 
+// MARK: - Получаем список репозиториев и выводим его на экран
 func getUserRepositoriesList(url: String) {
     print("Запрашиваю список репозиториев...")
     request(url, method: .get).validate().responseJSON { response in
+        // Проверяем что запрос выполнен успешно
         guard response.result.isSuccess else {
             print("Ошибка: \(response.result.error?.localizedDescription ?? "")")
             exit(0)
         }
-        guard let value = response.result.value as? [[String: Any]] else {
+        // Проверяем что получен именно список репозиториев, а не другие данные
+        guard let reposList = response.result.value as? [[String: Any]] else {
             print("Получены неверные данные")
             exit(0)
         }
-        for item in value {
-            guard let repoName = item["name"] as? String else { exit(0) }
-            print(repoName)
+        // Выводим список репозиториев на экран
+        print("Список получен:")
+        for item in reposList {
+            guard let reposName = item["name"] as? String else { exit(0) }
+            print(" \(reposName)")
         }
         exit(0)
     }
@@ -42,8 +47,5 @@ let userName = readName()
 urlString = urlString + userName + "/repos"
 getUserRepositoriesList(url: urlString)
 
+// Для того, чтобы программа не завершилась до получения ответа с сервера
 RunLoop.current.run()
-
-
-
-
